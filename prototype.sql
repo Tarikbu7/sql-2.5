cc64fb7ba50516f486d0d4313483d5bf3d3e4639
tarik <bufardiitarik@gmail.com>
2026-01-09 03:59:51 +0100

    Add files via upload

---
+++ prototype.sql
@@ -0,0 +1,40 @@
+--Question 1 : listez tous les articles avec leurs auteurs
+select articles.title, users.name
+from articles inner join  users
+on
+ articles.user_id= users.id
+
+
+--Question 2 : Afficher les articles écrits par un auteur spécifique (id 1 ou n’importe qui)
+select articles.title, users.name from articles
+inner join users
+on articles.user_id=users.id
+where users.id=1
+
+
+-- Question 3 : Afficher les articles avec leur prix total
+select articles.title,orders.total_price
+from articles inner join orders
+on
+articles.user_id=orders.user_id
+
+
+
+
+--Question 4 : Afficher les articles, les auteurs et les commentaires
+select articles.title, users.name, orders.total_price
+from articles inner join users
+on
+articles.user_id=users.id
+inner join orders
+on
+orders.user_id=users.id
+
+
+--"Show all articles with their author’s name and email
+select articles.user_id,title, users.name,email
+from articles inner join users
+on
+articles.user_id=users.id
+
+
